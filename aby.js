163d2651dc884c35460ff17a03eaef2b8d9cc487
HJP <49158393+hjpvue@users.noreply.github.com>
2020-05-19 10:58:57 +0200

    Update and rename 143089 to aby.js

---
+++ aby.js
@@ -0,0 +1,45 @@
+========================================================================================================
+---------------------------------------------------------------------------------aby.js-----------------
+
+========================================================================================================
+methods
+	import rat from rattle.js as rattler() => unit tester, makes lists of objects via tree shakes and
+        ranks them according to total number of methods + properties
+	taint = decorates/marks rat lists, inversely with timestamps, and proportionally to the rat rank
+        = more attachments>more time. Total obj+elem=timeout.main()
+	reap = deconstructs tainted, rattled elements and replaces their binary footprints with 0/1 
+        (1 is assumed to be more economically replaced with 0, rather than the inverse)
+
+function() = rat()
+	node.ENV.list-all()
+
+function() = taint()
+	for i : rat(), i ++;
+	.append.STRING.to rat();
+	where STRING = 8bit.Hash();
+return .obj => tainted.rat
+require.INTERVAL=(1%.os-cpu>()ms) run.reap();
+
+function() = reap()
+	create.uWebsocket(write.stream)(data=1);
+	fs.touch.overwrite(data);
+return stream.close(); 
+
+///Primitive Types
+///Rule of Four
+///Philosophical reasoning: Triplets, Triads, Tetrahedrons, Triangles and related tertially defined
+relationships all facilitate bi-directional communication without being dependant on any routing 
+and/or topological adjustments. Object Orientations and their associated frameworks loosely attach
+themselves to constructors, the proper inter-relation of which, is directed by behavioural bias.
+To address this, an error-prone zone is invoked so as to allow for the existence of a hidden node.
+
+const typechecker = {
+	'check.type(isRatlled)
+	'check.type(isChanted)
+	'check.type(isTainted)
+	'check.type(isRecursed)
+
+Rattled = { elements.array(distal)==elements.array(post) } === { Type=BOOLEAN }         ///]
+Chanted = INTEGER/number                                               ///]
+Tainted = FLOAT/percentage/modulus                                  ///]
+Recursed = STRING                                                                                                             ///]
